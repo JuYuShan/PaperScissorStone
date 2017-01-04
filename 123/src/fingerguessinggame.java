870e9f411416dba5f07c0b326d85a7d1813eab44
JuYuShan <105021085@live.asia.edu.tw>
2017-01-04 11:09:25 +0800

    paper
    ddd

--- 123/src/fingerguessinggame.java
+++ 123/src/fingerguessinggame.java
@@ -8,23 +8,33 @@ import java.util.Random;
 import javax.swing.*;
 public class fingerguessinggame {
 	static boolean st=false;
-	static JFrame frm = new JFrame("猜拳遊戲");
-	static Label Players=new Label("            玩家");
-	static Label computer=new Label("            電腦");
+	static JFrame frm = new JFrame("paper!scissors!stone!");
+	static Label Players=new Label("Player ",Label.CENTER);
+	static Label computer=new Label("Computer ",Label.CENTER);
 	static JButton Playersres = new JButton();
 	static JButton computerres= new JButton();
-	static Label judgeres = new Label("",Label.CENTER);
-	static Button end = new Button("結束");
-	static Button con = new Button("繼續");
+	static Label judgeres = new Label("",Label.CENTER);	
+	static Label NumberofBoard = new Label("Inning：",Label.CENTER);
+	static Label drawn = new Label("Tie：",Label.CENTER);
+	static Label Lose = new Label("You Lose：",Label.CENTER);
+	static Label Win = new Label("You win：",Label.CENTER);
+	static Button end = new Button("End");
+	static Button con = new Button("Continue");
 	static URL urlscissor,urlpaper,urlrock ;
 	static ImageIcon Scissor,paper,rock,PScissor,Ppaper,Prock;
 	static URL Purlscissor,Purlpaper,Purlrock ;
 	public static void main(String[] args) {
+		int C=1;
+		int b=0;
+		Label NumberofBoard1 = new Label(""+C,Label.CENTER);
+		Label drawn1 = new Label(""+b,Label.CENTER);
+		Label Lose1 = new Label(""+b,Label.CENTER);
+		Label Win1 = new Label(""+b,Label.CENTER);
 		computerres.setEnabled(false);
 		Playersres.setEnabled(false);
 		//視窗設置大小及顏色
 		frm.setSize(900,650);
-		frm.getContentPane().setBackground(Color.DARK_GRAY);
+		frm.getContentPane().setBackground(Color.cyan);
 		frm.setLayout(null);
 		try {
 			//電腦
@@ -50,13 +60,13 @@ public class fingerguessinggame {
 		//玩家設置
          Players.setBounds(150, 80, 200, 35);
          Players.setBackground(Color.BLACK);
-         Font fontplayer = new Font(Font.DIALOG, Font.BOLD, 20);
+         Font fontplayer = new Font(Font.DIALOG_INPUT, Font.BOLD, 20);
          Players.setFont(fontplayer);
          Players.setForeground(Color.white);
          //電腦設置
          computer.setBounds(550, 80, 200,35);
          computer.setBackground(Color.BLACK);
-         Font fontcomputer = new Font(Font.DIALOG, Font.BOLD, 20);
+         Font fontcomputer = new Font(Font.DIALOG_INPUT, Font.BOLD,20);
          computer.setFont(fontcomputer);
          computer.setForeground(Color.white);
          Font fontres = new Font(Font.DIALOG, Font.BOLD, 30);
@@ -88,15 +98,70 @@ public class fingerguessinggame {
 	    computerres.setBackground(Color.white);	
 	  	//輸贏顯示區
 	    judgeres.setBounds(205, 470,500, 50);
-	    judgeres.setBackground(Color.white);	
+	    judgeres.setBackground(Color.white);
+	    Font fontNumberofBoard7 = new Font(Font.DIALOG_INPUT, Font.BOLD, 30);
+	    judgeres.setFont(fontNumberofBoard7);
 	  	//結束按鈕
 	  	end.setBounds(250, 550, 150, 30);
 	  	end.setBackground(Color.BLACK);	
 	  	end.setForeground(Color.white);
+	  	Font fontNumberofBoard4 = new Font(Font.DIALOG_INPUT, Font.BOLD, 20 );
+	  	end.setFont(fontNumberofBoard4);
 	  	//繼續按鈕
 	  	con.setBounds(500, 550, 150, 30);
 	  	con.setBackground(Color.BLACK);	
 	  	con.setForeground(Color.white);
+	  	Font fontNumberofBoard6 = new Font(Font.DIALOG_INPUT, Font.BOLD, 20);
+	  	con.setFont(fontNumberofBoard6);
+	  	//局數
+	  	NumberofBoard.setBounds(80, 0, 90, 30);
+	  	NumberofBoard.setBackground(Color.cyan);	
+	  	NumberofBoard.setForeground(Color.BLACK);
+	  	Font fontNumberofBoard = new Font(Font.DIALOG_INPUT, Font.BOLD, 20 );
+	  	NumberofBoard.setFont(fontNumberofBoard);
+	  	NumberofBoard1.setBounds(170, 0, 35, 30);
+	  	NumberofBoard1.setBackground(Color.cyan);	
+	  	NumberofBoard1.setForeground(Color.BLACK);
+	  	Font fontNumberofBoard1 = new Font(Font.DIALOG, Font.BOLD, 20);
+	  	NumberofBoard1.setFont(fontNumberofBoard1);	  	
+	  	//贏的次數	  	
+	  	Win.setBounds(260, 0, 115, 30);
+		Win.setBackground(Color.cyan);	
+		Win.setForeground(Color.BLUE);
+	  	Font fontWin = new Font(Font.DIALOG_INPUT, Font.BOLD, 20);
+	  	Win.setFont(fontWin);
+	  	Win1.setBounds(375,0, 35, 30);
+		Win1.setBackground(Color.cyan);	
+		Win1.setForeground(Color.BLUE);
+	  	Font fontWin1 = new Font(Font.DIALOG, Font.BOLD, 20);
+	  	Win1.setFont(fontWin1);
+	  	
+	  	//輸的次數		
+		Lose.setBounds(480, 0, 120, 30);
+		Lose.setBackground(Color.cyan);	
+		Lose.setForeground(Color.RED);
+	  	Font fontLose = new Font(Font.DIALOG_INPUT, Font.BOLD, 20);
+	  	Lose.setFont(fontLose); 
+	  	Lose1.setBounds(605,0, 35, 30);
+	  	Lose1.setBackground(Color.cyan);	
+	  	Lose1.setForeground(Color.RED);
+	  	Font fontLose1 = new Font(Font.DIALOG, Font.BOLD, 20);
+	  	Lose1.setFont(fontLose1);
+		
+		//和局次數
+		drawn.setBounds(700, 0, 55, 30);
+		drawn.setBackground(Color.cyan);	
+		drawn.setForeground(Color.black);
+	  	Font fontdrawn = new Font(Font.DIALOG_INPUT, Font.BOLD,20);
+	  	drawn.setFont(fontdrawn); 
+	  	drawn1.setBounds(760, 0, 35, 30);
+		drawn1.setBackground(Color.cyan);	
+		drawn1.setForeground(Color.BLACK);
+	  	Font fontdrawn1 = new Font(Font.DIALOG, Font.BOLD,20);
+	  	drawn1.setFont(fontdrawn1); 
+		
+	  	
+		
 	  	rockjbutton2.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
@@ -106,15 +171,36 @@ public class fingerguessinggame {
 				int com;
 				com=rdm.nextInt(3);
 				showres(judgment(2,comturn(com)));
+				
 				//System.out.println(""+comturn(com));
 				paperjbutton.setEnabled(false);
 				Scissorjbutton1.setEnabled(false);
 				rockjbutton2.setEnabled(false);
+				if(comturn(com)==1)
+				{
+					int b=Integer.parseInt(Win1.getText());
+					b++;
+					Win1.setText(""+b);
+				}
+				else if(comturn(com)==2)
+				{
+					int b=Integer.parseInt(drawn1.getText());
+					b++;
+					drawn1.setText(""+b);
+				}
+				else
+				{
+					int b=Integer.parseInt(Lose1.getText());
+					b++;
+					Lose1.setText(""+b);
+				}
+				
 			}});
 	  	Scissorjbutton1.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				// TODO Auto-generated method stub
+				
 				Playersres.setIcon(Scissor);
 				st=true;
 				int com;
@@ -124,6 +210,24 @@ public class fingerguessinggame {
 				paperjbutton.setEnabled(false);
 				Scissorjbutton1.setEnabled(false);
 				rockjbutton2.setEnabled(false);
+				if(comturn(com)==0)
+				{
+					int b=Integer.parseInt(Win1.getText());
+					b++;
+					Win1.setText(""+b);
+				}
+				else if(comturn(com)==1)
+				{
+					int b=Integer.parseInt(drawn1.getText());
+					b++;
+					drawn1.setText(""+b);
+				}
+				else
+				{
+					int b=Integer.parseInt(Lose1.getText());
+					b++;
+					Lose1.setText(""+b);
+				}
 			}});
 	  	paperjbutton.addActionListener(new ActionListener(){
 			@Override
@@ -138,14 +242,44 @@ public class fingerguessinggame {
 				paperjbutton.setEnabled(false);
 				Scissorjbutton1.setEnabled(false);
 				rockjbutton2.setEnabled(false);
+				if(comturn(com)==2)
+				{
+					int b=Integer.parseInt(Win1.getText());
+					b++;
+					Win1.setText(""+b);
+				}
+				else if(comturn(com)==0)
+				{
+					int b=Integer.parseInt(drawn1.getText());
+					b++;
+					drawn1.setText(""+b);
+				}
+				else
+				{
+					int b=Integer.parseInt(Lose1.getText());
+					b++;
+					Lose1.setText(""+b);
+				}
 			}});
+	  
+	  	
 		frm.add(Players);
 		frm.add(computer);
 		frm.add(Playersres);
 		frm.add(computerres);
 		frm.add(judgeres);
 		frm.add(end);
+		frm.add(Win);
+		frm.add(drawn);		
+		frm.add(Lose);
+		frm.add(Win1);
+		frm.add(drawn1);	
+		frm.add(Lose1);
 		frm.add(con);
+		frm.add(NumberofBoard);
+		frm.add(NumberofBoard1);
+		
+		
 		frm.setVisible(true);
 		//按結束離開
 		end.addActionListener(new ActionListener(){
@@ -167,7 +301,7 @@ public class fingerguessinggame {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
+				// TODO Auto-generated method stub			
 				Playersres.setIcon(null);
 				computerres.setIcon(null);
 				judgeres.setText(null);
@@ -175,8 +309,14 @@ public class fingerguessinggame {
 				Scissorjbutton1.setEnabled(true);
 				rockjbutton2.setEnabled(true);
 				st=false;
+				int C=Integer.parseInt(NumberofBoard1.getText());
+				C++;
+				NumberofBoard1.setText(""+C);
+				
 			}});
 		animate();
+		
+		
 	}
 	//電腦出牌
 	public static int comturn(int i)
@@ -287,15 +427,18 @@ public class fingerguessinggame {
 		switch(i)
 		{
 			case 0:
-				judgeres.setText("勝利 ！！！！！");
+				judgeres.setText("You Win ! ! ! !");
+				
 				break;
 			case 1:
-				judgeres.setText("和局！！！！！");
+				judgeres.setText("The game has tie ! ! ! !");
 				break;
 			case 2:
-				judgeres.setText("敗北！！！！！");
+				judgeres.setText("You Lose ! ! ! !");
 				break;
 		}
 	}
+	
+	
 
 }
